1ba4d1bd546927c3bc85f15f30a14305dd389d7a
zhaohu <zhaohu@gridsum.com>
2022-04-12 09:41:20 +0800

    init

---
+++ concurrentcoding/src/main/java/com/zhaohu/concurrentcoding/ConcurrentcodingApplication.java
@@ -0,0 +1,13 @@
+package com.zhaohu.concurrentcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentcodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentcodingApplication.class, args);
+    }
+
+}
---
+++ concurrentcoding/src/test/java/com/zhaohu/concurrentcoding/ConcurrentcodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaohu.concurrentcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentcodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
